3ed8e5169a72890279f9ca009094cdc63656d30a
Miguel Lima <miguellimaoliver97@gmail.com>
2021-08-25 15:53:51 -0300

    Primeiro Commit Relógio

---
+++ projeto2/README.md
@@ -0,0 +1,5 @@
+HTML - Estrutura base montada do relógio - ( divs-ponteiros/riscos)
+
+Css - Em construção
+
+JS - Acrescentado Horas, minutos, segundos para o relógio digital ( 100% em funcionamento)
---
+++ projeto2/script.js
@@ -0,0 +1,22 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+}
+
+function fixZero(time) {
+return time < 10 ? `0${time}` : time;
+
+}
+
+
+
+setInterval(updateClock, 1000);
