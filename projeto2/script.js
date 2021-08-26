7cdaefdb643155332336409a36a1d7afa54fd257
Miguel Lima <miguellimaoliver97@gmail.com>
2021-08-26 14:05:39 -0300

    Adicionando commit final - mudanças no JS/CSS

--- projeto2/script.js
+++ projeto2/script.js
@@ -10,6 +10,15 @@ function updateClock() {
     let second = now.getSeconds();
 
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+     
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+    
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 }
 
 function fixZero(time) {
@@ -20,3 +29,4 @@ return time < 10 ? `0${time}` : time;
 
 
 setInterval(updateClock, 1000);
+updateClock();
